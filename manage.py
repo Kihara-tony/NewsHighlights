d74e41988a7fcd4ac1255866257945d6f36b1e13
Tony Kihara <tonykiharatonkin@gmail.com>
2019-06-21 23:27:18 +0300

    added the test function in manage.

--- manage.py
+++ manage.py
@@ -5,3 +5,11 @@ manager = Manager(app)
 manager.add_command('server',Server)
 @manager.command
 def test():
+    '''
+    Run test
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verboity=2).run(tests)
+if __name__ == '__main__':
+    manager.run()
