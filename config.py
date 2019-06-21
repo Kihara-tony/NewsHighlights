8e47da810d176e193b497845fa829a2bcb45d6b2
Tony Kihara <tonykiharatonkin@gmail.com>
2019-06-21 23:37:48 +0300

    added the secret key.

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 import os 
-
+class Config:
+    '''
+    Main configuration parent class
+    '''
+    NEWSHIGHLIGHTS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
+    NEWSHIGHLIGTS_API_KEY = os.environ.get('NEWSHIGHLIGHT_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
