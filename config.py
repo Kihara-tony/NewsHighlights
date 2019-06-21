969fa34bbcd94f5169e0000b9e042f08ad5c6604
Tony Kihara <tonykiharatonkin@gmail.com>
2019-06-21 23:45:27 +0300

    added the the production and development sudclasses.

--- config.py
+++ config.py
@@ -6,3 +6,18 @@ class Config:
     NEWSHIGHLIGHTS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
     NEWSHIGHLIGTS_API_KEY = os.environ.get('NEWSHIGHLIGHT_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    Args:
+        config: The parent configuration
+    '''
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    Args:
+        config:The parent configuration class
+    '''
+
+
