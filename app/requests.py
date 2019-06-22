c36ed524aff6485439b28ce9d8abd4d8866394c3
Tony Kihara <tonykiharatonkin@gmail.com>
2019-06-22 17:12:03 +0300

    added configuration of request

--- app/requests.py
+++ app/requests.py
@@ -1,2 +1,11 @@
 import urllib.request,json
 from .models import Source,Articles
+api_key = None
+base_url = None
+base_url_articles = None
+def configure_request(app):
+    global api_key,base_url,base_url_articles
+    api_key = app.config['NEWSHIGHLIGHT_API_KEY']
+    base_url = app.config['NEWSHIGHLIGHT_API_BASE_URL']
+    base_url_articles = app.config['NEWSHIGHLIGHT_API_ARTICLE_URL']
+    
