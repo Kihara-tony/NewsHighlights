76a362fdeba0696dbe198c1cb653f5331ad0e1df
Tony Kihara <tonykiharatonkin@gmail.com>
2019-06-22 17:21:38 +0300

    added a function of getting json request

--- app/requests.py
+++ app/requests.py
@@ -8,4 +8,16 @@ def configure_request(app):
     api_key = app.config['NEWSHIGHLIGHT_API_KEY']
     base_url = app.config['NEWSHIGHLIGHT_API_BASE_URL']
     base_url_articles = app.config['NEWSHIGHLIGHT_API_ARTICLE_URL']
-    
+def get_sources():
+    '''
+    Function that get the json request
+    '''
+    get_sources_url = base_url.format(api_key)
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+        sources_results = None
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_results(sources_results_list)
+    return sources_results
