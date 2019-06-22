997feb114730c744b8ed36adea1b384e7b0ab18b
Tony Kihara <tonykiharatonkin@gmail.com>
2019-06-22 16:34:54 +0300

    added the function of articles.

--- app/main/views.py
+++ app/main/views.py
@@ -9,3 +9,12 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+@main.route('/article/<string:id>')
+def source(id):
+    '''
+    View source page function that shows its source and details
+    '''
+    articles = get_articles(id)
+    print(articles)
+    return render_template('articles.html',articles=articles)
+    
